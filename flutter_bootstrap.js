d78b0d3cbc889e7b009e9355173e29bad59bdc0c
Dapo-dan <Dapo-dan@users.noreply.github.com>
2025-01-11 06:58:42 +0000

    deploy: 25afb999325b6d042f20a72b97db6b84d409083b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2366496109"
+    serviceWorkerVersion: "358646731"
   }
 });
